c1b9e43209fc9577b59226d333d8a8790def1476
aerouk <xcraaze@gmail.com>
2013-12-09 23:41:29 +0000

    Adding doc to main class + initialise travis-ci

--- src/main/java/us/kpvpdev/easycommands/EasyCommands.java
+++ src/main/java/us/kpvpdev/easycommands/EasyCommands.java
@@ -4,6 +4,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import us.kpvpdev.easycommands.utils.init.InitCommands;
 
+/**
+ * EasyCommands - A Bukkit plugin to aid players and staff alike.
+ *
+ * @author iEpix
+ * @version 0.0.1
+ */
 public class EasyCommands extends JavaPlugin implements Listener
 {
     @Override
